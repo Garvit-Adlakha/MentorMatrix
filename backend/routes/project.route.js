184c091cd23ea89839fb1ede2f3094ac09082457
Garvit Adlakha <garvitadlakha08@gmail.com>
2025-02-12 04:03:38 +0000

    add routes

--- backend/routes/project.route.js
+++ backend/routes/project.route.js
@@ -1,4 +1,4 @@
-import { addMemberToProject, createProject } from "../controllers/project.controller.js";
+import { addMemberToProject, createProject, getProject, listProjects, mentorDecision, requestMentor } from "../controllers/project.controller.js";
 import { Router } from "express";
 import { isAuthenticated } from "../middleware/auth.middleware.js";
 import { validateDescription } from "../middleware/validation.middleware.js";
@@ -10,4 +10,8 @@ const router=Router()
 //authenticated route
 router.post("/create-project",isAuthenticated,validateDescription,createProject)
 router.post("/add-members",isAuthenticated,addMemberToProject)
+router.post('/request-mentor',isAuthenticated,requestMentor)
+router.post('/assign-mentor/:projectId',isAuthenticated,mentorDecision)
+router.get('/get-project',isAuthenticated,getProject)
+router.get('/list-projects',isAuthenticated,listProjects)
 export default router
