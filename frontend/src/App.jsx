35a00c1d7536ddad996c5982fa19d7a8a497eedf
Garvit Adlakha <garvitadlakha08@gmail.com>
2025-03-18 15:15:30 +0530

    Refactor App component to use React Router for routing and remove unused CSS file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { lazy, Suspense } from "react"
 
-function App() {
+// ... your lazy imports ...
+
+
+
+// Move this to AuthContext
+const user =true
+
+const App = () => {
   return (
-    <div className="">
-      <button className="btn">Button</button>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<Home />} />
+          
+          {/* 404 route */}
+          <Route path="*" element={<>Not Found</>} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
