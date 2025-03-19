581d886e491dc61ca0c5fac53ad9bd22ca1ba415
Garvit Adlakha <garvitadlakha08@gmail.com>
2025-03-19 15:33:57 +0530

    feat: update HomePage to include Footer component and render Navbar

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import Main from '../Components/shared/Main'
-
+import Footer from '../Components/shared/footer'
 import Navbar from '../Components/shared/Navbar'
 const HomePage = () => {
   return (
     <>
+    <Navbar />
     <Main />
     <Footer />
     </>
