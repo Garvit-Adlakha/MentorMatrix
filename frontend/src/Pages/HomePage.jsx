e20f7a39e43054ad3045585ad04c82fbfa8a4e5c
Garvit Adlakha <garvitadlakha08@gmail.com>
2025-03-21 10:23:07 +0530

    feat: implement FacultyCards and FacultyCard components; update imports for consistency; add Login component

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 import Main from '../components/shared/Main'
 import Footer from '../components/shared/footer'
 import Navbar from '../components/shared/Navbar'
-import FacultyCards from '../Components/FacultyCards'
+import FacultyCards from '../components/FacultyCards'
 
 const HomePage = () => {
   return (
@@ -11,19 +10,7 @@ const HomePage = () => {
       <Main />
       <FacultyCards />
       <Footer />
-=======
-import Main from '../Components/shared/Main'
-import Footer from '../Components/shared/footer'
-import Header from '../Components/shared/Header'
-const HomePage = () => {
-  return (
-    <>
-    <Header />
-    <Main />
-    <Footer />
->>>>>>> 8622a6d5c817b1f2a1df6dca854395eb7fb8d2e0
     </>
   )
 }
-
 export default HomePage
