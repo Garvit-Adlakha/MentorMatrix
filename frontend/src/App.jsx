8a9d2852bb1dac82a05c03af0336a0f3b4d5f3c7
Garvit Adlakha <garvitadlakha08@gmail.com>
2025-04-02 10:44:05 +0530

    update app to use router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { lazy, Suspense } from "react"
+import AppRouter from "./router"
 
-// Lazy load page components
-const HomePage = lazy(() => import("./Pages/HomePage"))
-const Login = lazy(() => import("./components/auth/Login"))
-const Signup = lazy(() => import("./components/auth/Signup"))
-const MentorPage=lazy(()=> import("./Pages/MentorPage"))
 // Move this to AuthContext
 const user = true
 
@@ -13,18 +9,7 @@ const App = () => {
   return (
     <>
     <BrowserRouter>
-      <Suspense fallback={<div className="flex items-center justify-center min-h-screen">Loading...</div>}>
-        <Routes>
-          {/* Public routes */}
-          <Route path="/" element={<HomePage />} />
-          <Route path="/mentor" element={<MentorPage />} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<Signup />} />
-          
-          {/* 404 route */}
-          <Route path="*" element={<>Not Found</>} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </BrowserRouter>
     </>
   )
